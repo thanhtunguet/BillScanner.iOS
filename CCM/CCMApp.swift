867fa0523f35d62f6d2f670a63b8cb19f0dcca3d
thanhtunguet <thanhtung.uet@gmail.com>
2024-05-18 16:52:26 +0700

    Initial Commit

---
+++ CCM/CCMApp.swift
@@ -0,0 +1,17 @@
+//
+//  CCMApp.swift
+//  CCM
+//
+//  Created by Thanh Tùng Phạm on 18/5/24.
+//
+
+import SwiftUI
+
+@main
+struct CCMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
